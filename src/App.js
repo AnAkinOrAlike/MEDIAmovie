9e122695e30a453543e1a91fd34af01589ae4f2d
AnAkinOrAlike <andresacm166@gmail.com>
2025-11-05 17:04:58 -0400

    App.js: añadido fetch para los registros con marcha, añadido grid personalizado (bhbs) que imprime segun si es en visto (funcion GridHeaderBoxSmall), en marcha o ninguno, modificado fila 3 del grid, los registros de duracion y ultimo visto fueron movidos al header, en la fila 3 fue añadido el pais de origen, añadida función para la barra de progreso.
    Css: limpiado clases no usadas y movidas las que van a otros lugares.

--- src/App.js
+++ src/App.js
@@ -5,30 +5,38 @@ import './main.css';
 import {Helmet} from "react-helmet";
 import { useState, useEffect } from 'react';
 import { supabase } from './supabase.js';
-import { LibraryAdd, DarkMode, Visibility } from '@nine-thirty-five/material-symbols-react/sharp';
+import { LibraryAdd, DarkMode, Visibility, Planet, MotionPlay } from '@nine-thirty-five/material-symbols-react/sharp';
+import { BookmarkHeart } from "@nine-thirty-five/material-symbols-react/sharp/filled";
 
 function App() {
   const [media, setMedia] = useState([]);
   const [theme, setTheme] = useState('dark');
-  const main_limite = 10;
+  const main_limite = 20;
 
   const fetchData = async () => {
-  const { data, error } = await supabase
-    .from("mediavistos")
-    .select(`*`);
-  
-  if (error) console.error(error);
-  else console.log(data); 
+    const { data: vistos, error: errorVistos } = await supabase
+      .from("mediavistos")
+      .select("*");
 
-  function shuffle(arr) {
-    return arr
-      .map((item) => ({ sort: Math.random(), value: item }))
-      .sort((a, b) => a.sort - b.sort)
-      .map((obj) => obj.value);
-  }
-  const randomized = shuffle(data);
-  setMedia(randomized);
-  console.log(randomized);
+    const { data: marcha, error: errorMarcha } = await supabase
+      .from("mediamarcha")
+      .select("*");
+
+    if (errorVistos) throw errorVistos;
+    if (errorMarcha) throw errorMarcha;
+
+    const combined = [...(vistos || []), ...(marcha || [])];
+
+    function shuffle(arr) {
+      return arr
+        .map((item) => ({ sort: Math.random(), value: item }))
+        .sort((a, b) => a.sort - b.sort)
+        .map((obj) => obj.value);
+    }
+
+    const randomized = shuffle(combined);
+    setMedia(randomized);
+    console.log(randomized);
   };
 
   function GridItem({ media }) {
@@ -38,35 +46,60 @@ function App() {
     return (
       <div className="gridvisto" style={{ "--main-color": mainColor }}>
         <div className="ghti" id="headline" style={{ backgroundColor: mainColor }}> {media?.nombre || "..."}
-        <div className="rhyr">                                                      {media?.year || "..."}{" "}{media?.duracion || "..."}{" "}{media?.typedur || "..."}
+        <div className="rhyr">                                                      <Planet height={20} width={20} viewBox="0 -1100 900 900" className="over"/>
+                                                                                    {media?.year || "..."}{" "}
+                                                                                    <MotionPlay height={20} width={20} viewBox="0 -1100 900 900" className="over"/>
+                                                                                    {media?.duracion || "..."}{" "}{media?.typedur || "..."}
         </div></div>
 
-        <div className="ghbs" id="box" style={{ backgroundColor: mainColor }}>      <StarRating rating={rating} mainColor={mainColor} />
+        <div className="ghbs" style={{ backgroundColor: mainColor }}>              <GridHeaderBoxSmall media={media} rating={rating} mainColor={mainColor} />
         </div>
 
-        <div className="gbim">                                                      <img className="bima gl" src={media?.imagen || logo} alt="logo" />
+        <div className="gbim">                                                      <img className="bima popGlow" src={media?.imagen || logo} alt="logo" />
         </div>
 
         <div className="gr1f">                                                      Director:
-        <br /><span className="colorSpan">                                          {media?.director || "..."}
+        <br /><span className="bwbk">                                          {media?.director || "..."}
         </span></div>
 
         <div className="gr2f">                                                      Distribuidora:
-        <br /><span className="colorSpan">                                          {media?.compania || "..."}
+        <br /><span className="bwbk">                                          {media?.compania || "..."}
         </span></div>
 
-        <div className="gr3l"></div>
-
-        <div className="gr3r"><span className="colorSpan">                          {media?.categoria || "..."}{" "}{media?.catvisto || "..."} el {media?.ultimo_visto || "00/00/0000"}
+        <div className="gr3f">                                                      País:
+        <br /> <span className="bwbk">                                          {media?.country || "..."}
         </span></div>
       </div>
     );
   }
 
+  function GridHeaderBoxSmall({ media, rating, mainColor }) {
+  if (media?.stars) {
+    return (
+      <>                                                        {media?.categoria || "..."} {media?.catvisto || "..."}
+        <br /><span className="bwbk">                      {media?.ultimo_visto || "00/00/0000"}
+        </span>                                                 <StarRating rating={rating} mainColor={mainColor} />
+      </>
+    );
+  } else if (media?.avance && media.avance !== 0) {
+    return (
+      <>                                                        {media?.categoria || "..."} {media?.catvisto || "..."}
+      <br /><span className="bwbk">                        {media?.ultimo_visto || "00/00/0000"}
+      </span>                                                   <ProgressBar value={media.avance} mainColor={mainColor} />
+      </>
+    );
+  } else {
+    return (
+      <>                                                           Actualizar Serie:
+      <br />
+      <div className="bstr popGlow" style={{ "--letterSpa": "normal" }}><BookmarkHeart height={30} width={30} />
+      </div></>
+    );}}
+
 
   function StarRating({ rating, mainColor }) {
     return (
-      <div className="bstr gl">
+      <div className="bstr popGlow" style={{ "--letterSpa": "3px" }}>
         {[1, 2, 3, 4, 5].map(num => (<React.Fragment key={num}>
           <input
             id={`radio${num}`} type="radio" value={num} className="none" checked={rating === num} readOnly/>
@@ -77,6 +110,21 @@ function App() {
             ★
           </label>
         </React.Fragment>))}
+      </div>
+    );
+  }
+
+  function ProgressBar({ value, mainColor }) {
+    const progress = Math.max(0, Math.min(100, value));
+    const totalBlocks = 8;
+    const filledBlocks = Math.round((progress / 100) * totalBlocks);
+    const bar = Array.from({ length: totalBlocks }, (_, i) =>
+    i < filledBlocks ? "■" : "□"
+  );
+    return (
+      <div className="bstr popGlow" style={{ "--letterSpa": "-2px" }}>
+        {bar}
+        <span >{progress}%</span>
       </div>
     );
   }
@@ -103,17 +151,15 @@ return (
 
   <header className='HeaderBack'>
     <div className="HeaderWelcome"> ¡BIENVENIDO A MI PROGRAMA!</div>
-    <button className="HeaderButtons gl active"> <Visibility height={40} width={40}/> </button>
-    <button className="HeaderButtons gl"> <LibraryAdd height={40} width={40}/></button>
-    <button className={`HeaderButtons gl ${theme === "dark" ? "active" : ""}`} onClick={toggleTheme}> <DarkMode height={40} width={40}/></button>
+    <button className="HeaderButtons popGlow active"> <Visibility height={40} width={40}/> </button>
+    <button className="HeaderButtons popGlow"> <LibraryAdd height={40} width={40}/></button>
+    <button className={`HeaderButtons popGlow ${theme === "dark" ? "active" : ""}`} onClick={toggleTheme}> <DarkMode height={40} width={40}/></button>
     <div className="HeaderLabels"> Lista para Ver</div>
     <div className="HeaderLabels"> Añadir Nuevo</div>
     <div className="HeaderLabels"> Modo Oscuro</div>
   </header>
   <main>
-
     <span className="divider">Media</span>
-
     <div className='flexTable'>
     {media.slice(0, main_limite).map((media) => (
       <GridItem key={`${media.nombre}-${media.year}`} media={media} />
