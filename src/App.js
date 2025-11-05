ada735cd0a5ffc68c00635d98af2421466fa720b
AnAkinOrAlike <andresacm166@gmail.com>
2025-11-04 23:32:18 -0400

    Instalado @nine-thirty-five/material-symbols-react (para utilizar iconos) y react-helmet (para reemplazar head) añadidos views mediavistos y mediamarcha en la base de datos para hacer fetch directamente y no seleccionar cada tabla en el codigo, añadido un modo oscuro, añadido header completado al 100% añadido divisor entre header y body
    en css:
    añadido modo luz y oscuro basico
    modificado table -> flextable
    funcion los botones activos con color diferente
    filas del grid arregladas para que sean fracciones y no cm
    stfill y star reemplazados por bstr (box star)
    añadido divisor entre header y body

--- src/App.js
+++ src/App.js
@@ -1,153 +1,125 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import './main.css';
+import {Helmet} from "react-helmet";
 import { useState, useEffect } from 'react';
 import { supabase } from './supabase.js';
-
-console.log(process.env.REACT_APP_SUPABASE_URL);
-console.log(process.env.REACT_APP_SUPABASE_ANON_KEY);
+import { LibraryAdd, DarkMode, Visibility } from '@nine-thirty-five/material-symbols-react/sharp';
 
 function App() {
   const [media, setMedia] = useState([]);
+  const [theme, setTheme] = useState('dark');
+  const main_limite = 10;
 
   const fetchData = async () => {
   const { data, error } = await supabase
-    .from("MEDIA")
-    .select(`
-      id,
-      nombre, 
-      year, 
-      duracion, 
-      imagen, 
-      colorhex,
-      visto,
-      categoria:CATEGORIA ( duracion, categoria ), 
-      artist:DIRECTORES ( artist ), 
-      hogar:COMPAÑIA ( hogar )
-    `);
+    .from("mediavistos")
+    .select(`*`);
+  
+  if (error) console.error(error);
+  else console.log(data); 
 
-  if (error) {
-    console.error("Error fetching data:", error.message);
-    return;
+  function shuffle(arr) {
+    return arr
+      .map((item) => ({ sort: Math.random(), value: item }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((obj) => obj.value);
   }
-
-  const procesadas = await Promise.all(
-    data.map(async (m) => {
-      let vistos = null;
-
-      if (m.visto === false) {
-        const { data: enMarcha } = await supabase
-        .from("MARCHA")
-        .select("*")
-        .eq("id_media", m.id)
-        .single();
-
-        vistos = { tipo: "marcha", ...enMarcha }; 
-
-      } else {
-        const { data: enVisto } = await supabase
-        .from("VISTOS")
-        .select("stars, ultimo_visto")
-        .eq("id_media", m.id)
-        .single();
-
-        let fechaFormateada = null;
-
-        if (enVisto?.ultimo_visto) {
-          const d = new Date(enVisto.ultimo_visto);
-          const dia = String(d.getDate()).padStart(2, "0");
-          const mes = String(d.getMonth() + 1).padStart(2, "0");
-          const año = d.getFullYear();
-          fechaFormateada = `${dia}/${mes}/${año}`;
-        }
-
-        vistos = { tipo: "visto", ...enVisto, ultimo_visto: fechaFormateada };
-      }
-
-  return { ...m, vistos };}));
-  setMedia(procesadas);
-  console.log(procesadas);
+  const randomized = shuffle(data);
+  setMedia(randomized);
+  console.log(randomized);
   };
 
-function GridItem({ media }) {
-  const mainColor = media?.colorhex || "#6495ed";
-  const rating = media?.vistos?.stars || 0;
+  function GridItem({ media }) {
+    const mainColor = media?.colorhex || "#6495ed";
+    const rating = media?.stars || 0;
 
-  return (
-    <div className="gridvisto">
-      <div className="ghti" id="headline" style={{ backgroundColor: mainColor }}> {media?.nombre || "..."}
-      <div className="rhyr">                                                      {media?.year || "..."}
-      </div></div>
+    return (
+      <div className="gridvisto" style={{ "--main-color": mainColor }}>
+        <div className="ghti" id="headline" style={{ backgroundColor: mainColor }}> {media?.nombre || "..."}
+        <div className="rhyr">                                                      {media?.year || "..."}{" "}{media?.duracion || "..."}{" "}{media?.typedur || "..."}
+        </div></div>
 
-      <div className="ghbs" id="box" style={{ backgroundColor: mainColor }}>      <StarRating rating={rating} mainColor={mainColor} />
+        <div className="ghbs" id="box" style={{ backgroundColor: mainColor }}>      <StarRating rating={rating} mainColor={mainColor} />
+        </div>
+
+        <div className="gbim">                                                      <img className="bima gl" src={media?.imagen || logo} alt="logo" />
+        </div>
+
+        <div className="gr1f">                                                      Director:
+        <br /><span className="colorSpan">                                          {media?.director || "..."}
+        </span></div>
+
+        <div className="gr2f">                                                      Distribuidora:
+        <br /><span className="colorSpan">                                          {media?.compania || "..."}
+        </span></div>
+
+        <div className="gr3l"></div>
+
+        <div className="gr3r"><span className="colorSpan">                          {media?.categoria || "..."}{" "}{media?.catvisto || "..."} el {media?.ultimo_visto || "00/00/0000"}
+        </span></div>
       </div>
+    );
+  }
 
-      <div className="gbim">                                                      <img className="bima gl" src={media?.imagen} alt="logo" />
+
+  function StarRating({ rating, mainColor }) {
+    return (
+      <div className="bstr gl">
+        {[1, 2, 3, 4, 5].map(num => (<React.Fragment key={num}>
+          <input
+            id={`radio${num}`} type="radio" value={num} className="none" checked={rating === num} readOnly/>
+          <label
+            htmlFor={`radio${num}`}
+            id={`star${num}`}
+            style={{ color: num <= rating ? mainColor : "#000" }}>
+            ★
+          </label>
+        </React.Fragment>))}
       </div>
+    );
+  }
 
-      <div className="gr1f">                                                      Director:
-      <br /><span id="dir" style={{ color: mainColor }}>                          {media?.artist?.artist || "..."}
-      </span></div>
+  useEffect(() => {
+      fetchData();
+    }, []); 
+  
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+    document.body.className = theme;
+  };
 
-      <div className="gr2f">                                                      Distribuidora:
-      <br /><span id="dis" style={{ color: mainColor }}>                          {media?.hogar?.hogar || "..."}
-      </span></div>
-
-      <div className="gr3l">                                                      Dura {media?.duracion || "..."}{" "}
-      <span id="vis" style={{ color: mainColor }}>                                {media?.categoria?.duracion || "..."}
-      </span></div>
-
-      <div className="gr3r">
-      <span id="vis" style={{ color: mainColor }}>                               {media?.categoria?.categoria || "..."} vista el:
-      </span><br />                                                              {media?.vistos?.ultimo_visto || "00/00/0000"}
-      </div>
-    </div>
-  );
-}
-const main_limite = 3;
-
-function StarRating({ rating, mainColor }) {
-  return (
-    <div className="stfill star gl" id="star">
-      {[1, 2, 3, 4, 5].map(num => (<>
-        <input
-          id={`radio${num}`} type="radio" name="estrellas" value={num} className="none" checked={rating === num} readOnly/>
-        <label
-          htmlFor={`radio${num}`}
-          id={`star${num}`}
-          style={{ color: num <= rating ? mainColor : "#000" }}>
-          ★
-        </label>
-      </>))}
-    </div>
-  );
-}
-
- useEffect(() => {
-    fetchData();
-  }, []); 
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
 return (
   <>
-  <head>
+  <Helmet>
     <meta charSet="UTF-8" />
     <title>Base representada</title>
-  </head>
+  </Helmet>
 
   <header className='HeaderBack'>
-    <div className="HeaderWelcome"> Bienvenido a mi programa</div>
-    <div className="HeaderButtons"> Lista para Ver</div>
-    <div className="HeaderButtons"> Añadir Nuevo</div>
-    <div className="HeaderButtons"> Modo Día o Noche</div>
+    <div className="HeaderWelcome"> ¡BIENVENIDO A MI PROGRAMA!</div>
+    <button className="HeaderButtons gl active"> <Visibility height={40} width={40}/> </button>
+    <button className="HeaderButtons gl"> <LibraryAdd height={40} width={40}/></button>
+    <button className={`HeaderButtons gl ${theme === "dark" ? "active" : ""}`} onClick={toggleTheme}> <DarkMode height={40} width={40}/></button>
     <div className="HeaderLabels"> Lista para Ver</div>
     <div className="HeaderLabels"> Añadir Nuevo</div>
-    <div className="HeaderLabels"> Modo Día o Noche</div>
+    <div className="HeaderLabels"> Modo Oscuro</div>
   </header>
-
   <main>
+
+    <span className="divider">Media</span>
+
+    <div className='flexTable'>
     {media.slice(0, main_limite).map((media) => (
-      <GridItem key={media.id} media={media} />
+      <GridItem key={`${media.nombre}-${media.year}`} media={media} />
     ))}
+    </div>
+    
   </main>
   </>
 )
